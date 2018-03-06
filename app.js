0cec2a5004ab612782c0fbb48a5df89095aa5e13
junholee <gomjunho@naver.com>
2018-03-06 17:15:40 +0900

    test git

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 app.get('/', function(req, res){
-	res.send('test');
+	res.send('test git');
 });
 app.listen(3000, function(){
 	console.log('Connecting 3000 port.....');
